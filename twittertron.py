de90a885d4b717d6b768512dbede8f6ec0447755
Ben Nuttall <ben@bennuttall.com>
2014-09-16 18:14:09 +0100

    Import tweepy and auth tokens

---
+++ twittertron.py
@@ -0,0 +1,14 @@
+import tweepy
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
