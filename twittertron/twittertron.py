ce964133a8a08f98b482492fd09433070fbdc528
Ben Nuttall <ben@bennuttall.com>
2014-09-18 11:16:39 +0100

    Use joystick to update display

--- twittertron/twittertron.py
+++ twittertron/twittertron.py
@@ -1,14 +1,22 @@
 import twitter
-from dot3k import lcd, backlight
+from dot3k import lcd, backlight, joystick
 from random import random
 
-def main():
-    followers = twitter.follower_count()
-
-    msg = "You have %-6i followers" % followers
+@joystick.on(joystick.UP)
+def show_follower_count(pin):
+    try:
+        followers = twitter.follower_count()
+        msg = "You have %-6i followers" % followers
+    except:
+        msg = "Twitter API     limit reached"
 
     backlight.sweep(random())
+    lcd.clear()
     lcd.write(msg)
+
+def main():
+    while True:
+        pass
 
 if __name__ == '__main__':
     main()
