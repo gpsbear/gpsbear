d83292f0187a916f5ba2b2858e2d072900de2c5b
Ben Nuttall <ben@bennuttall.com>
2014-09-18 11:14:50 +0100

    Add random backlight sweep

--- twittertron/twittertron.py
+++ twittertron/twittertron.py
@@ -1,11 +1,13 @@
 import twitter
-from dot3k import lcd
+from dot3k import lcd, backlight
+from random import random
 
 def main():
     followers = twitter.follower_count()
 
     msg = "You have %-6i followers" % followers
 
+    backlight.sweep(random())
     lcd.write(msg)
 
 if __name__ == '__main__':
