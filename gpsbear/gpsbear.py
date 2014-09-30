1c8632c54a06d90c8c93e046e7b3494d18abe5e2
cloeee <clmc-x@hotmail.com>
2014-09-30 14:13:06 +0100

    add text overlay

--- gpsbear/gpsbear.py
+++ gpsbear/gpsbear.py
@@ -1,15 +1,27 @@
 import twitter
 import picamera
 from datetime import datetime
+from random import choice
+
+messages = [
+    "Don't Touch me!",
+    "Call Bear Line!",
+     "I don't like it when you touch me that way!",
+    "Help me!",
+    "BABBAGE BEAR NO LIKEY!",
+    "Why are you doing this?!",
+]
 
 def main():
     with picamera.PiCamera() as camera:
         camera.start_preview()
         timestamp = datetime.now().isoformat()
         img = '/home/pi/twitter/%s.jpg' % timestamp
+        message = choice(messages)
+        camera.annotate_text = message
         camera.capture(img)
         camera.stop_preview()
-        twitter.tweet("hello", img)
+        twitter.tweet(message, img)
 
 if __name__ == '__main__':
     main()
