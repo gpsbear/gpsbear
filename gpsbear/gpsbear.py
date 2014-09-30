1e84d689d49056280675083c41866bf79049dd1a
cloeee <clmc-x@hotmail.com>
2014-09-30 12:51:30 +0100

    Remove displayatron code

--- gpsbear/gpsbear.py
+++ gpsbear/gpsbear.py
@@ -1,22 +1,8 @@
 import twitter
-from dot3k import lcd, backlight, joystick
-from random import random
-
-@joystick.on(joystick.UP)
-def show_follower_count(pin):
-    try:
-        followers = twitter.follower_count()
-        msg = "You have %-6i followers" % followers
-    except:
-        msg = "Twitter API     limit reached"
-
-    backlight.sweep(random())
-    lcd.clear()
-    lcd.write(msg)
 
 def main():
-    while True:
-        pass
+    follower_count = twitter.follower_count()
+    print("You have %s followers" % follower_count)
 
 if __name__ == '__main__':
     main()
