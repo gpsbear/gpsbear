97f8a1150ddb1d5fe4f959a66c4a809f492fd446
michelle4803mi <michelle4803@gmail.com>
2014-09-30 15:10:00 +0100

    Use axcellerometer as photo/tweet trigger

--- gpsbear/gpsbear.py
+++ gpsbear/gpsbear.py
@@ -2,10 +2,7 @@ import twitter
 import picamera
 from datetime import datetime
 from random import choice
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
+import adxl345
 
 messages = [
     "Don't Touch me!",
@@ -16,17 +13,39 @@ messages = [
     "Why are you doing this?!",
 ]
 
-def main():
+def take_picture_and_tweet():
     with picamera.PiCamera() as camera:
         camera.start_preview()
         timestamp = datetime.now().isoformat()
         img = '/home/pi/twitter/%s.jpg' % timestamp
         message = choice(messages)
         camera.annotate_text = message
-        GPIO.wait_for_edge(17, GPIO.FALLING)
         camera.capture(img)
         camera.stop_preview()
         twitter.tweet(message, img)
+
+def main():
+    accel = adxl345.ADXL345()
+    accel.setRange(adxl345.RANGE_16G)
+    xold = 0
+    yold = 0
+    zold = 0
+    while True:
+        axes = accel.getAxes(True)
+        xnew = axes["x"]
+        ynew = axes["y"]
+        znew = axes["z"]
+
+        if float(xnew) - float(xold) > 2 or float(xnew) - float(xold) < - 2:
+            take_picture_and_tweet()
+        elif float(ynew) - float(yold) > 2 or float(ynew) - float(yold) < - 2:
+            take_picture_and_tweet()
+        elif float(znew) - float(zold) > 2 or float(znew) - float(zold) < - 2:
+            take_picture_and_tweet()
+
+        xold = xnew
+        yold = ynew
+        zold = znew
 
 if __name__ == '__main__':
     main()
